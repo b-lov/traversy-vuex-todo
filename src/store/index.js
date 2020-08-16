f33f9f469c3f2c120bb6e82d4484f10a3f5d28c2
maxpiano <maxbilov@gmail.com>
2020-08-16 23:48:14 +0200

    get data from jsonplaceholder into vues store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import todos from './modules/todos';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    todos
   }
-})
+});

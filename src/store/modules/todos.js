c1f88fc28ee1ed9ebab3ff936fb133d4bbf0cfa5
maxpiano <maxbilov@gmail.com>
2020-08-17 16:51:23 +0200

    finished

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -34,6 +34,13 @@ const actions = {
       `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
     );
     commit('setTodos', response.data);
+  },
+  async updateTodo({ commit }, updTodo) {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${updTodo.id}`,
+      updTodo
+    );
+    commit('updTodo', response.data);
   }
 };
 
@@ -41,7 +48,13 @@ const mutations = {
   setTodos: (state, todos) => (state.todos = todos),
   newTodo: (state, todo) => state.todos.unshift(todo),
   removeTodo: (state, id) =>
-    (state.todos = state.todos.filter(todo => todo.id !== id))
+    (state.todos = state.todos.filter(todo => todo.id !== id)),
+  updTodo: (state, updTodo) => {
+    const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+    if (index !== -1) {
+      state.todos.splice(index, 1, updTodo);
+    }
+  }
 };
 
 export default {
